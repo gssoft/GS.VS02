faec2419b356c90253cfd05496d1f3ce1721c3b6
gs_soft <gs_soft@mail.ru>
2026-07-22 03:58:20 +0300

    NamedPipesSln01, Quote.AppHost

---
+++ 2606/Aspire/DeepSeek/AspireNamedPipesApp02/Quote/Quote.AppHost/AppHost.cs
@@ -0,0 +1,9 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+builder.AddProject<Projects.QuoteServer>("quoteserver");
+
+builder.AddProject<Projects.QuoteClient1>("quoteclient1");
+
+builder.AddProject<Projects.QuoteClient2>("quoteclient2");
+
+builder.Build().Run();
---
+++ 2606/Aspire/DeepSeek/AspireNamedPipesApp02/Quote/QuoteClient2/Program.cs
@@ -0,0 +1,9 @@
+using QuoteClient2;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.AddServiceDefaults();
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
