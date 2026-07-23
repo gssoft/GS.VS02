94abac6f31e15a237e1ec266636ce1c8e46830f6
gs_soft <gs_soft@mail.ru>
2026-07-24 02:42:41 +0300

    QuoteServer

--- 2606/Aspire/DeepSeek/AspireNamedPipesApp02/Quote/Quote.AppHost/Program.cs
+++ 2606/Aspire/DeepSeek/AspireNamedPipesApp02/Quote/Quote.AppHost/Program.cs
@@ -3,13 +3,30 @@
 var server = builder.AddProject<Projects.QuoteServer>("quoteserver")
     .WithEnvironment("SERVER_PORT", "5555");
 
-// Передаём клиентам адрес сервера (внутри Aspire все видят друг друга по именам)
 builder.AddProject<Projects.QuoteClient1>("quoteclient1")
-    .WithEnvironment("SERVER_HOST", "quoteserver")
+    .WithEnvironment("SERVER_HOST", "localhost")
     .WithEnvironment("SERVER_PORT", "5555");
 
 builder.AddProject<Projects.QuoteClient2>("quoteclient2")
-    .WithEnvironment("SERVER_HOST", "quoteserver")
+    .WithEnvironment("SERVER_HOST", "localhost")
     .WithEnvironment("SERVER_PORT", "5555");
 
 builder.Build().Run();
+
+
+// 26.07.20
+//var builder = DistributedApplication.CreateBuilder(args);
+
+//var server = builder.AddProject<Projects.QuoteServer>("quoteserver")
+//    .WithEnvironment("SERVER_PORT", "5555");
+
+//// Передаём клиентам адрес сервера (внутри Aspire все видят друг друга по именам)
+//builder.AddProject<Projects.QuoteClient1>("quoteclient1")
+//    .WithEnvironment("SERVER_HOST", "quoteserver")
+//    .WithEnvironment("SERVER_PORT", "5555");
+
+//builder.AddProject<Projects.QuoteClient2>("quoteclient2")
+//    .WithEnvironment("SERVER_HOST", "quoteserver")
+//    .WithEnvironment("SERVER_PORT", "5555");
+
+//builder.Build().Run();
