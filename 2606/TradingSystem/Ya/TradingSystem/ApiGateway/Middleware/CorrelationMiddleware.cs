0f864544fd8aa4669f5fb6363fe1544e4e7056e3
gs_soft <gs_soft@mail.ru>
2026-08-27 15:57:20 +0300

    The End

---
+++ 2606/TradingSystem/Ya/TradingSystem/ApiGateway/Middleware/CorrelationMiddleware.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiGateway.Middleware
+{
+    internal class CorrelationMiddleware
+    {
+    }
+}
---
+++ 2606/TradingSystem/Ya/TradingSystem/TradingSystem.Tests/Infrastructure/TestAppHostFixture.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingSystem.Tests.Infrastructure
+{
+    internal class TestAppHostFixture
+    {
+    }
+}
