dbff5729095833a1af08c776a7d24d77798b2b25
gs_soft <gs_soft@mail.ru>
2026-08-27 15:21:56 +0300

    Strategy.Executer

---
+++ 2606/TradingSystem/Ya/TradingSystem/Portfolio.Manager/Controllers/PositionsController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portfolio.Manager.Controllers
+{
+    internal class PositionsController
+    {
+    }
+}
---
+++ 2606/TradingSystem/Ya/TradingSystem/Portfolio.Manager/Services/DealCalculatorService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portfolio.Manager.Services
+{
+    internal class DealCalculatorService
+    {
+    }
+}
