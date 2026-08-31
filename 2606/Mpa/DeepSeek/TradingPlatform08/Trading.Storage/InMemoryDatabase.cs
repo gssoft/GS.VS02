9c587d9e91d0d6da80fa90bd1ec4329d1f1bea7a
gs_soft <gs_soft@mail.ru>
2026-08-31 17:53:04 +0300

    TradingPlatform08 new sln

---
+++ 2606/Mpa/DeepSeek/TradingPlatform08/Trading.Storage/InMemoryDatabase.cs
@@ -0,0 +1,22 @@
+﻿// InMemoryDatabase.cs
+using System.Collections.Concurrent;
+using Trading.Domain;
+
+namespace Trading.Storage;
+
+public class InMemoryDatabase
+{
+    public ConcurrentDictionary<Guid, OrderCreated> Orders { get; } = new();
+    public ConcurrentDictionary<Guid, Trade> Trades { get; } = new();
+    public ConcurrentDictionary<string, PositionUpdated> Positions { get; } = new();
+
+    // Новая коллекция для хранения последних событий (для UI)
+    public ConcurrentQueue<object> EventLog { get; } = new();
+
+    public void SaveOrder(OrderCreated order) => Orders[order.OrderId] = order;
+    public void SaveTrade(Trade trade) => Trades[trade.TradeId] = trade;
+    public void UpdatePosition(PositionUpdated position) => Positions[position.Ticker] = position;
+
+    // Метод для логирования событий
+    public void LogEvent(object evt) => EventLog.Enqueue(evt);
+}
